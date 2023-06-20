ea413943ab4f85e40d30ec9e502e255a59536070
alphasr <nilaybaranwal@gmail.com>
2023-06-20 19:44:43 +0200

    first commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,8 +8,37 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token }) {
+      if (token?.email === 'nilaybaranwal@gmail.com') {
+        token.role = 'admin';
+      } else {
+        token.role = 'member';
+      }
+      return token;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
